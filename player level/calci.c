bf7974edd6d50127d2a144c3168d3df16dd564a7
meerak05 <38652894+meerak05@users.noreply.github.com>
2018-04-24 04:40:11 +0000

    Create calci.c

---
+++ player level/calci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int add,sub,div,mul;
+  int a=5,b=5;
+  add=a+b;
+  sub=a-b;
+  div=a/b;
+  mul=a*b;
+  printf("%d\n",add);
+  printf("%d\n",sub);
+  printf("%d\n",div);
+  printf("%d\n",mul);
+  return 0;
+}  
