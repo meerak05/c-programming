c600fcd791f4d16aa1cd643772904b5442a8980d
meerak05 <38652894+meerak05@users.noreply.github.com>
2018-04-24 04:42:51 +0000

    Create name.c

---
+++ Beginner level/name.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char name[25]="Sachin Tendulkar";
+char nickname[25]="Little Master";
+int runsscored=18426;
+float average=44.83;
+int matches=463;
+printf("%s\n",name);
+printf("%s\n",nickname);
+printf("%d\n",runsscored);
+printf("%0.2f\n",average);
+printf("%d\n",matches);
+return 0;
+}
